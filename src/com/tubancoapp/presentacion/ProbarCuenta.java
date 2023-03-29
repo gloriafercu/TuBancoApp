68f7ed5b6224ec8ff5368893aec693f586f55ddc
gloriafercu <gloriafercu@gmail.com>
2023-03-29 11:31:05 +0200

    add new comments to exercise

--- src/com/tubancoapp/presentacion/ProbarCuenta.java
+++ src/com/tubancoapp/presentacion/ProbarCuenta.java
@@ -7,7 +7,7 @@ import com.tubancoapp.modelo.CuentaNomina;
 public class ProbarCuenta {
     public static void main(String[] args) {
 
-        /* Creamos una cuenta Ahorro */
+        /* 1. Creamos una cuenta Ahorro */
 
         System.out.println("-----CUENTA AHORRO-----");
         Cuenta ca1 = new CuentaAhorro("Pedro", "12345678A", "C/Margarita 15, Madrid", 4500.90, 2000.45, 567.4);
@@ -16,13 +16,14 @@ public class ProbarCuenta {
         System.out.println("Cuenta Ahorro ca2: " + ca2);
         System.out.println("El saldo calculado de la cuentaAhorro ca2 es: " + ca2.calcularSaldo());
         System.out.println("El saldo calculado de la cuentaAhorro con Intereses ca2 es: " + ca2.calcularSaldo(2));
-        // Modifique el valor del dni del cliente con el setter
+
+        // Modifico el valor del dni del cliente con el setter
         System.out.println("El dni del cliente cuentaAhorro ca2 es: " + ca2.getDni());
         ca2.setDni("9999999A");
         // Obtengo el nuevo valor del dni el cliente con el getter
         System.out.println("El dni del cliente cuentaAhorro ca2 después de modificación: " + ca2.getDni());
 
-        /* Creamos una cuenta Nómina */
+        /* 2. Creamos una cuenta Nómina */
 
         System.out.println("-----CUENTA NÓMINA-----");
         Cuenta cn1 = new CuentaNomina("Luis", "12345678B", "C/Febrero 23, Madrid", 15678.99, 3450.65, 1235.66, "123456FF67LKX", "Accenture", 500);
@@ -30,8 +31,11 @@ public class ProbarCuenta {
         CuentaNomina cn2 = (CuentaNomina) cn1;
         System.out.println("Cuenta Nomina cn2: " + cn2);
         System.out.println("El saldo calculado de la cuentaNomina cn2 es: " + cn2.calcularSaldo());
+
+        // Modifico el valor de la comisión con el setter
         System.out.println("La comisión del cliente cuentaNomina cn2 después: " + cn2.getComision());
         cn2.setComision(10000);
+        // Obtengo el nuevo valor de la comisión con el getter y después el nuevo saldo de la cuenta
         System.out.println("La comisión del cliente cuentaNomina cn2 después de modificación: " + cn2.getComision());
         System.out.println("Nuevo saldo: " + cn2.calcularSaldo());
 
